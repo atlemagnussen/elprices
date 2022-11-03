7a4d4ffd1296be708d2e07eca5626c303b429f53
Atle Magnussen <atlemagnussen@gmail.com>
2022-11-03 14:01:59 +0100

    testing promise

--- svelte-kit/src/routes/+page.svelte
+++ svelte-kit/src/routes/+page.svelte
@@ -2,12 +2,37 @@
 	<title>Home</title>
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
+<script lang="ts">
 
+
+	let valueDate = new Date()
+
+	let promise: Promise<Date> | null
+	let load = () => {
+		if (!promise) {
+			promise = new Promise((resolve, reject) => {
+				setTimeout(() => {
+					resolve(new Date())
+				}, 1000)
+			})
+		}
+
+		promise.then(value => {
+			console.log(value)
+			valueDate = value
+		})
+	}
+
+</script>
 <section>
 	<p>Hello</p>
 	<!-- <li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}> -->
 	<p>
 		<a href="/prices">Prices</a>
+		<br>
+		<br>
+		valueDate: {valueDate} <br>
+		<button on:click="{load}">Load</button>
 	</p>
 </section>
 
